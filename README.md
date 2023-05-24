5e095efbd57a4a22f065901f60a0104adbc03fa6
Knievil <105438364+Knievil@users.noreply.github.com>
2023-05-24 15:14:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-h1astl)
---
+++ index.js
@@ -0,0 +1,53 @@
+// Import stylesheets
+import './style.css';
+
+// lesson 1 //
+
+/*let myVariable = 321;
+const MY_VARIABLE = 123;
+
+('this is a string');
+('so is this');
+
+let myString = 'this is a string value stored in a variable wowee';
+
+let myString1 = 'Hello';
+let myString2 = 'World';
+
+myString1 + myString2 + ':3'; //HelloWorld:3
+myString1 + ' ' + myString2; //Hello World
+myString1 + ' ' + myString2 + ' ' + ':3'; //Hello World :3
+
+//
+
+let myTrueBool = true;
+let myFalseBool = false;
+
+// lession 1 part 2 //
+
+let name = 'Sam';
+function displayGreeting(name) {
+	const message = 'Howdy, ${name}!';
+	console.log(message);
+}
+
+displayGreeting(name);
+*/
+
+// LESSON TWO
+
+/*
+let myTrueBool = true
+let myFalseBool = false
+
+let currentMoney;
+let fursuitPrice;
+let suitDiscountPrice = fursuitPrice - (fursuitPrice * .20)
+
+if (currentMoney >= fursuitPrice || currentMoney >= suitDiscountPrice){
+  console.log("new suit");
+}
+
+else{
+  console.log("oof poor");
+}*/
